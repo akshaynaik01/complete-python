de47e89378e29478c21ff8a38f5c91b9ef926ff4
Akshay Naik <akshaynk292006@gmail.com>
2026-04-06 17:51:13 +0530

    Add comments and variable/data type examples
    Added comments, variable examples, and data type demonstrations in class-1.py.

---
+++ class-1.py
@@ -0,0 +1,48 @@
+# Comments
+
+# single line commnets 
+
+"""Multi line
+commnets"""
+
+
+# Variables
+
+a = 12
+b = 13
+print(a, b)
+
+# Name conventions in variavles
+
+#1.Camel Case
+myName = "Akshay Naik"
+
+#2.Pascal Case
+MyName = "Akshay Naik"
+
+#3.Snake Case
+my_name = "Akshay Naik"
+
+print(myName, MyName, my_name)
+
+
+# Data Types
+
+  #1. Numbers
+a = 17
+print(type(a))  # integer 
+
+b = 17.5
+print(type(b)) #float
+
+c = 2 + 3j
+print(type(c)) # complex 
+
+#2. String
+name = "I am Akshay"
+print(name , type(name))
+
+#3. Boolean
+a = True 
+b = False
+print(a,b , type(a))
