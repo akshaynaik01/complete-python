3037ad4bb5e5f3e931c51b37740a37815d976134
Akshay Naik <akshaynk292006@gmail.com>
2026-04-07 21:24:37 +0530

    Create class-3.py

---
+++ class-3.py
@@ -0,0 +1,29 @@
+              ##  ____----- ARITHMETIC OPERATOR -----___  ##
+# OPERATORS - " +, -, *, /, //, **, % ""
+
+
+a = 30
+b = 18
+
+print(a+b) #48
+print(a-b) #12
+print(a*b) #540
+print(a/b) #1.66
+print(a//b) #1
+print(a**b) #387420
+print(a%b)  #12
+
+
+a = 10
+b = 4
+print(a*b+a-b//a/b) # apply bodmas - 50.0
+
+a = 20
+a += 10  # a = a + 10
+print(a)
+
+# String 
+a = "Hello"
+b = " World"
+print(a+b) # concatenation
+print(a*3) # multiplication
